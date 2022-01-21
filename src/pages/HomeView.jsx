09150da23ac86a791fd17e734e10fbd6c71857a0
Oleksandr Balitskiy <popingalov@gmail.com>
2022-01-21 20:18:48 +0300

    123

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,9 +1,18 @@
 import Container from '../Components/Container/Container';
-
+import './homePage.css';
 export default function HomeView() {
   return (
     <Container>
-      <div>Здесь может быть ваша реклама!</div>
+      <main className="main">
+        <h1>Phonebook</h1>
+        <p>Здесь может быть ваша реклама! Ну а пока здесь будет HTML ёлочка)</p>
+        <div className="tree">
+          <div className="tree-layer"></div>
+          <div className="tree-layer"></div>
+          <div className="tree-layer"></div>
+          <div className="tree-log"></div>
+        </div>
+      </main>
     </Container>
   );
 }
